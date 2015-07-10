d6feff8e1072f832ecc1c2da4ee1c86fdfe3ebe6
umbobabo <umbertobabini@economist.com>
2015-07-10 16:48:37 +0100

    Improved example

--- example.js
+++ example.js
@@ -2,5 +2,8 @@ import GobbetCollection from './index';
 import React from 'react';
 
 export default (
-  <GobbetCollection></GobbetCollection>
+  <div className="no-touch">
+    <h2 style={{ border: 'solid red 1px' }}>Attention this component use .no-touch and .touch class by Modernizr</h2>
+    <GobbetCollection />
+  </div>
 );
--- index.js
+++ index.js
@@ -4,55 +4,43 @@ import ImageCaption from '@economist/component-imagecaption'
 import ShareBar from '@economist/component-sharebar';
 import Icon from '@economist/component-icon';
 
-class WorldIfGobbet extends React.Component {
-
+export default class GobbetCollection extends React.Component {
   render() {
     let customHeader = <ShareBar useFX={true} fxDirection="flip-to-top" fxType="cube" background="#333333" fxDefaultStateBackground="#999999"/>;
     return (
-      <Gobbet className="gobbet" showShareBar={false} title={customHeader}>
-        <ImageCaption caption="Lorem ipsum dolor sit amet, consectetuer ad." src="http://lorempixel.com/g/300/169/food" textposition="top" />
-      </Gobbet>
-      );
-  }
-};
-
-export default class GobbetCollection extends React.Component {
-
-  render() {
-    return (
-      <div className="span_12">
+          <div className="span_12">
         <div className="section group span_10">
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
         </div>
         <div className="section group span_10">
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
           <div className="col span_4">
-            <WorldIfGobbet className="gobbet" showShareBar={true} >
+            <Gobbet className="gobbet" title={customHeader}>
               <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </WorldIfGobbet>
+            </Gobbet>
           </div>
         </div>
       </div>
