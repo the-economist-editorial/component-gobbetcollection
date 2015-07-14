fc21e646c8c6a39f44d3a7310bc0ff4cb8f7e9b1
russEco <russstreet@economist.com>
2015-07-14 11:25:04 +0100

    updated devpack, added assets, linted es6...

--- index.js
+++ index.js
@@ -1,49 +1,111 @@
-import React from 'react';
-import Gobbet from '@economist/component-gobbet'
-import ImageCaption from '@economist/component-imagecaption'
-import ShareBar from '@economist/component-sharebar';
-import Icon from '@economist/component-icon';
+'use strict';
 
-export default class GobbetCollection extends React.Component {
-  render() {
-    let customHeader = <ShareBar useFX={true} fxDirection="flip-to-top" fxType="cube" background="#333333" fxDefaultStateBackground="#999999"/>;
-    return (
-          <div className="span_12">
-        <div className="section group span_10">
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-        </div>
-        <div className="section group span_10">
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-          <div className="col span_4">
-            <Gobbet className="gobbet" title={customHeader}>
-              <ImageCaption caption="Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," src="http://lorempixel.com/g/300/169/food" textposition="top" />
-            </Gobbet>
-          </div>
-        </div>
-      </div>
-    )
+exports.__esModule = true;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _economistComponentGobbet = require('@economist/component-gobbet');
+
+var _economistComponentGobbet2 = _interopRequireDefault(_economistComponentGobbet);
+
+var _economistComponentImagecaption = require('@economist/component-imagecaption');
+
+var _economistComponentImagecaption2 = _interopRequireDefault(_economistComponentImagecaption);
+
+var _economistComponentSharebar = require('@economist/component-sharebar');
+
+var _economistComponentSharebar2 = _interopRequireDefault(_economistComponentSharebar);
+
+var _economistComponentIcon = require('@economist/component-icon');
+
+var _economistComponentIcon2 = _interopRequireDefault(_economistComponentIcon);
+
+var GobbetCollection = (function (_React$Component) {
+  function GobbetCollection() {
+    _classCallCheck(this, GobbetCollection);
+
+    _React$Component.apply(this, arguments);
   }
-}
+
+  _inherits(GobbetCollection, _React$Component);
+
+  GobbetCollection.prototype.render = function render() {
+    var customHeader = _react2['default'].createElement(_economistComponentSharebar2['default'], { useFX: true, fxDirection: 'flip-to-top', fxType: 'cube', background: '#333333', fxDefaultStateBackground: '#999999' });
+    return _react2['default'].createElement(
+      'div',
+      { className: 'span_12' },
+      _react2['default'].createElement(
+        'div',
+        { className: 'section group span_10' },
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        ),
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        ),
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        ),
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        ),
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        ),
+        _react2['default'].createElement(
+          'div',
+          { className: 'col span_4' },
+          _react2['default'].createElement(
+            _economistComponentGobbet2['default'],
+            { className: 'gobbet', title: customHeader },
+            _react2['default'].createElement(_economistComponentImagecaption2['default'], { caption: 'Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', src: 'http://lorempixel.com/g/300/169/food', textposition: 'top' })
+          )
+        )
+      )
+    );
+  };
+
+  return GobbetCollection;
+})(_react2['default'].Component);
+
+exports['default'] = GobbetCollection;
+module.exports = exports['default'];
+
